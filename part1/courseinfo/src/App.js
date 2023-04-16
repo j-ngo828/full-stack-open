d562a73807d40ab1578781bff9298c8a964cd6ff
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-04-16 15:24:33 -0400

    Finish 1.2 courseinfo part 2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,17 @@
 const Header = (props) => <h1>{props.title}</h1>
 
-const Content = (props) => <p>{props.text} {props.num}</p>
+const Part = (props) => <p>{props.text} {props.num}</p>
+
+const Content = (props) => {
+  const {data} = props
+  return (
+    <div>
+      <Part text={data[0].text} num={data[0].num} />
+      <Part text={data[1].text} num={data[1].num} />
+      <Part text={data[2].text} num={data[2].num} />
+    </div>
+  )
+}
 
 const Total = (props) => <p>Number of exercises {props.count}</p>
 
@@ -12,13 +23,26 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  
+  const data = [
+    {
+      text: part1,
+      num: exercises1,
+    },
+    {
+      text: part2,
+      num: exercises2,
+    },
+    {
+      text: part3,
+      num: exercises3,
+    }
+  ]
 
   return (
     <div>
       <Header title={course} />
-      <Content text={part1} num={exercises1} />
-      <Content text={part2} num={exercises2} />
-      <Content text={part3} num={exercises3} />
+      <Content data={data} />
       <Total count={exercises1 + exercises2 + exercises3} />
     </div>
   )
