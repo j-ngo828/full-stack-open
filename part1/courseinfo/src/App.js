1cb03ec3038ff7473e5e878f804f796bfc3f8ef9
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-04-16 18:49:32 -0400

    Finish 1.3 courseinfo part 3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,11 +4,12 @@ const Part = (props) => <p>{props.text} {props.num}</p>
 
 const Content = (props) => {
   const {data} = props
+  console.log(data);
   return (
     <div>
-      <Part text={data[0].text} num={data[0].num} />
-      <Part text={data[1].text} num={data[1].num} />
-      <Part text={data[2].text} num={data[2].num} />
+      <Part text={data[0].name} num={data[0].exercises} />
+      <Part text={data[1].name} num={data[1].exercises} />
+      <Part text={data[2].name} num={data[2].exercises} />
     </div>
   )
 }
@@ -17,33 +18,30 @@ const Total = (props) => <p>Number of exercises {props.count}</p>
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   
   const data = [
-    {
-      text: part1,
-      num: exercises1,
-    },
-    {
-      text: part2,
-      num: exercises2,
-    },
-    {
-      text: part3,
-      num: exercises3,
-    }
+    part1,
+    part2,
+    part3,
   ]
 
   return (
     <div>
       <Header title={course} />
       <Content data={data} />
-      <Total count={exercises1 + exercises2 + exercises3} />
+      <Total count={data.reduce((acc, part) => part.exercises + acc, 0)} />
     </div>
   )
 }
