27b29b2e20aa1d654cadd80a9872ded4fb166be5
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-04-18 18:11:53 -0400

    Finish 2.1 courseinfo step 6

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,25 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {
+      parts.map((part) =>
+        <Part key={part.id} part={part} />
+      )
+    }
+  </>
+
+const Course = ({course}) =>
+  <>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+  </>
+
+export default Course
