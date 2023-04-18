f35a724d9c96926c63856e6d4e5e9cf61010cfe4
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-04-18 18:20:18 -0400

    Finish 2.2 courseinfo step 7 and 2.3 courseinfo step 8

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,25 +1,13 @@
-const Header = ({ course }) => <h1>{course}</h1>
+import Content from './Content'
+import Header from './Header'
+import Total from './Total'
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {
-      parts.map((part) =>
-        <Part key={part.id} part={part} />
-      )
-    }
-  </>
 
 const Course = ({course}) =>
   <>
     <Header course={course.name} />
     <Content parts={course.parts} />
+    <Total sum={course.parts.reduce((acc, part) => acc + part.exercises, 0)} />
   </>
 
 export default Course
