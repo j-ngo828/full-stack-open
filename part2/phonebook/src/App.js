8a06173d14ef5e73c6f12471e73927608a185350
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-04-19 18:13:13 -0400

    Finish exercise 2.11 phonebook step 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,13 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
-const getDefaultPersonsState = () => ([
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-])
+const personApiRoute = 'http://localhost:3001/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState(getDefaultPersonsState()) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameSearchQuery, setNameSearchQuery] = useState('')
@@ -47,6 +43,16 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
+
+  useEffect(
+    () => {
+      console.log('effect');
+      axios
+        .get(personApiRoute)
+        .then((response) => setPersons(response.data))
+    },
+    [],
+  )
 
   return (
     <div>
