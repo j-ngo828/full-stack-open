198b886bb922d17ca2102390543afc29a13f820f
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-04-27 21:54:02 -0400

    Finish exercise 3.9 phonebook backend step 9

--- part3/phonebook-frontend/src/services/phonebookService.js
+++ part3/phonebook-frontend/src/services/phonebookService.js
@@ -1,20 +1,16 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons';
 
-const getAllContacts = () => axios.get(baseUrl).then((response) => response.data)
+const getAllContacts = () => axios.get(baseUrl).then((response) => response.data);
 
-const createContact = (newContact) => axios.post(baseUrl, newContact).then((response) =>
-  response.data
-)
+const createContact = (newContact) =>
+  axios.post(baseUrl, newContact).then((response) => response.data);
 
 const updateContact = (contactId, contactDetail) =>
-  axios.put(`${baseUrl}/${contactId}`, contactDetail).then((response) =>
-    response.data
-  )
+  axios.put(`${baseUrl}/${contactId}`, contactDetail).then((response) => response.data);
 
-const deleteContact = (contactId) => axios.delete(`${baseUrl}/${contactId}`).then((response) =>
-  response.data
-)
+const deleteContact = (contactId) =>
+  axios.delete(`${baseUrl}/${contactId}`).then((response) => response.data);
 
-export default {getAllContacts, createContact, deleteContact, updateContact}
+export default { getAllContacts, createContact, deleteContact, updateContact };
