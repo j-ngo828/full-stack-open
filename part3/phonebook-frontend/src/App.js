571962f130de40ff740bc6e2e5ffe9ae2eea907b
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-04-30 14:42:37 -0400

    Finish exercise 3.19 phonebook database step 7

--- part3/phonebook-frontend/src/App.js
+++ part3/phonebook-frontend/src/App.js
@@ -1,96 +1,102 @@
-import { useEffect, useState } from 'react'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-import phonebookService from './services/phonebookService'
-import Notification from './components/Notification'
+import { useEffect, useState } from 'react';
+import Filter from './components/Filter';
+import Notification from './components/Notification';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
+import phonebookService from './services/phonebookService';
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [nameSearchQuery, setNameSearchQuery] = useState('')
-  const [message, setMessage] = useState(null)
-  const [hasError, setHasError] = useState(false)
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [nameSearchQuery, setNameSearchQuery] = useState('');
+  const [message, setMessage] = useState(null);
+  const [hasError, setHasError] = useState(false);
 
-  useEffect(
-    () => {
-      phonebookService.getAllContacts()
-        .then((contacts) =>
-          setPersons(contacts)
-        )
-        .catch((error) => console.error(error))
-    },
-    [],
-  )
+  useEffect(() => {
+    phonebookService
+      .getAllContacts()
+      .then((contacts) => setPersons(contacts))
+      .catch((error) => console.error(error));
+  }, []);
 
   const getIndexOfPersonWithName = (submittedName) =>
-    persons.findIndex((person) => person.name === submittedName)
+    persons.findIndex((person) => person.name === submittedName);
 
-  const handleNameInputChange = (event) => setNewName(event.target.value)
+  const handleNameInputChange = (event) => setNewName(event.target.value);
 
-  const handleNumberInputChange = (event) => setNewNumber(event.target.value)
+  const handleNumberInputChange = (event) => setNewNumber(event.target.value);
 
-  const handleNameQueryInputChange = (event) => setNameSearchQuery(event.target.value)
+  const handleNameQueryInputChange = (event) => setNameSearchQuery(event.target.value);
 
   const handleSuccessfulOperation = (message) => {
-    setMessage(message)
+    setMessage(message);
     setTimeout(() => {
-      setMessage(null)
-    }, 5000)
-    return phonebookService.getAllContacts()
-  }
+      setMessage(null);
+    }, 5000);
+    return phonebookService.getAllContacts();
+  };
 
   const handleAddContact = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const newPerson = {
       name: newName,
       number: newNumber,
-    }
-    const nameExistsIndex = getIndexOfPersonWithName(newName)
+    };
+    const nameExistsIndex = getIndexOfPersonWithName(newName);
     if (nameExistsIndex !== -1) {
       const shouldReplaceContact = window.confirm(
         `${newName} is already added to the phonebook, replace the old number with a new one?`
-      )
+      );
       if (shouldReplaceContact) {
-        phonebookService.updateContact(persons[nameExistsIndex].id, newPerson)
+        phonebookService
+          .updateContact(persons[nameExistsIndex].id, newPerson)
           .then((_) => handleSuccessfulOperation(`Updated ${newName}'s number`))
           .then((contacts) => setPersons(contacts))
           .catch((error) => {
-            setHasError(true)
-            setMessage(`Information of ${newName} has already been removed from server`)
+            setHasError(true);
+            setMessage(error.response.data.error);
+            console.error(error);
             setTimeout(() => {
-              setMessage(null)
-              setHasError(false)
-            }, 5000)
-            phonebookService.getAllContacts().then((contacts) =>
-              setPersons(contacts)
-            )
-            console.error(error)
-          })
+              setHasError(false);
+              setMessage(null);
+            }, 5000);
+            phonebookService.getAllContacts().then((contacts) => setPersons(contacts));
+          });
       }
     } else {
-      phonebookService.createContact({...newPerson, id: persons.length + 1})
+      phonebookService
+        .createContact({ ...newPerson, id: persons.length + 1 })
         .then((_) => handleSuccessfulOperation(`Added ${newName}`))
         .then((contacts) => setPersons(contacts))
-        .catch((error) => console.error(error)) 
-      }
-      setNewName('')
-      setNewNumber('')
-  }
+        .catch((error) => {
+          setHasError(true);
+          setMessage(error.response.data.error);
+          setTimeout(() => {
+            setHasError(false);
+            setMessage(null);
+          }, 5000);
+          console.error(error);
+        });
+    }
+    setNewName('');
+    setNewNumber('');
+  };
 
   const handleDeleteContact = (contactId) => {
-    phonebookService.deleteContact(contactId)
-      .then((_) =>
-        phonebookService.getAllContacts()
-      )
-      .then((contacts) => setPersons(contacts))
-  }
+    phonebookService
+      .deleteContact(contactId)
+      .then((_) => phonebookService.getAllContacts())
+      .then((contacts) => setPersons(contacts));
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Notification message={message} isError={hasError} />
+      <Notification
+        message={message}
+        isError={hasError}
+      />
       <Filter
         nameSearchQuery={nameSearchQuery}
         handleNameQueryInputChange={handleNameQueryInputChange}
@@ -110,7 +116,7 @@ const App = () => {
         handleDeleteContact={handleDeleteContact}
       />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
