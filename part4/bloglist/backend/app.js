9b8f80ca8b5bf76fb954d46876843413ed0e3d30
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-04-30 18:54:51 -0400

    Finish 4.1 bloglist step 2

---
+++ part4/bloglist/backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+const { MONGODB_URI } = require('./utils/config')
+
+const app = express()
+
+logger.info('connecting to', MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
