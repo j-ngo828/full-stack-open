64d0cf31b94f6aaf4a0622b2f0b0a7e7daa93dab
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-05-04 21:50:24 -0400

    Finish exercise 4.20 bloglist expansion step 8

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -13,16 +13,8 @@ blogsRouter.get('/:id', async (request, response) => {
   response.json(oneBlog)
 })
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response) => {
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
