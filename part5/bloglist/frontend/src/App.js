11117184d4596a497bfd46dde3f51f0ef36e3033
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-05-07 16:11:36 -0400

    Finish exercise 5.3 bloglist frontend step 3

--- part5/bloglist/frontend/src/App.js
+++ part5/bloglist/frontend/src/App.js
@@ -3,11 +3,18 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+const getInitialBlogInputs = () => ({
+  title: '',
+  author: '',
+  url: '',
+})
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [blogInputs, setBlogInputs] = useState(getInitialBlogInputs())
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
@@ -18,6 +25,7 @@ const App = () => {
     if (loggedInUserJSON) {
       const userDetails = JSON.parse(loggedInUserJSON)
       setUser(userDetails)
+      blogService.setToken(userDetails.token)
     }
   }, [])
 
@@ -45,6 +53,14 @@ const App = () => {
     setUser(null)
   }
 
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+    console.log(blogInputs)
+    const createdBlog = await blogService.create(blogInputs)
+    setBlogs([...blogs, createdBlog])
+    setBlogInputs(getInitialBlogInputs())
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -68,6 +84,42 @@ const App = () => {
       <button type="submit">login</button>
     </form>
   )
+
+  const blogForm = () => {
+    return (
+      <form onSubmit={handleCreateBlog}>
+        <h2>create new</h2>
+        <div>
+          title
+          <input
+            type="text"
+            value={blogInputs.title}
+            name="Title"
+            onChange={({ target }) => setBlogInputs({ ...blogInputs, title: target.value })}
+          />
+        </div>
+        <div>
+          author
+          <input
+            type="text"
+            value={blogInputs.author}
+            name="Author"
+            onChange={({ target }) => setBlogInputs({ ...blogInputs, author: target.value })}
+          />
+        </div>
+        <div>
+          url
+          <input
+            type="text"
+            value={blogInputs.url}
+            name="Url"
+            onChange={({ target }) => setBlogInputs({ ...blogInputs, url: target.value })}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    )
+  }
 
   const renderBlogs = () =>
     blogs.map((blog) => (
@@ -96,7 +148,14 @@ const App = () => {
           {user.name} logged in {logOutButton()}
         </p>
       )}
-      {user ? renderBlogs() : loginForm()}
+      {user ? (
+        <>
+          {blogForm()}
+          <div>{renderBlogs()}</div>
+        </>
+      ) : (
+        loginForm()
+      )}
     </div>
   )
 }
--- part5/bloglist/frontend/src/services/blogs.js
+++ part5/bloglist/frontend/src/services/blogs.js
@@ -3,14 +3,22 @@ const baseUrl = '/api/blogs'
 
 let token = null
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then((response) => response.data)
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
 }
 
 const setToken = (newToken) => {
   token = `Bearer ${newToken}`
 }
 
+const create = async (payload) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.post(baseUrl, payload, config)
+  return response.data
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, setToken }
+export default { getAll, setToken, create }
